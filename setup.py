81c0c8cb747a9a1fd59d6ad87640865da8bc5fff
Regina Celi da Silva <regicelis7@hotmail.com>
2023-01-03 16:04:37 -0300

    set up projet inital organization, add setup.py to turn installable, add __main_ for executable, add __init__ for package, closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="reward system",
+    author="Regina Celi da Silva",
+    packages=find_packages(),
+)
