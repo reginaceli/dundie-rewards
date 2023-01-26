37cba53f4204cfbeea493834daeda725ea2e2241
Regina Celi da Silva <regicelis7@hotmail.com>
2023-01-26 19:10:48 -0300

    Adding finalizers, setup and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -6,9 +8,15 @@ medium: Medium priority
 low: Low priority
 """
 
-# hook
-
 
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+@pytest.fixture(autouse=True)
+# objeto request (injeção de dependência)
+def go_to_tmpdir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield  # protocolo generators
